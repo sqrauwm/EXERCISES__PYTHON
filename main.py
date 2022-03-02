ca3e01aa0fe9b2895841a61e5433b409dfd7dbc2
Arranasi <radio.gimbus@gmail.com>
2022-03-02 10:30:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,103 @@
+# a = 6
+# b = 6
+# if a > b:
+#     print('liczba a jest wieksza od liczby b')
+# elif a < b:
+#     print('liczba a jest mniejsza od liczby b')
+# else:
+#     print('liczba a jest rowna liczbie b')
+
+
+
+# a = input('wczytaj liczbę: ')
+# print(a)
+# print(type(a))
+# a = int(a)
+# print(type(a))
+
+
+# a = input('wczytaj pierwszą liczbę: ')
+# b = input('wczytaj drugą liczbę: ')
+# c = input('wczytaj trzecią liczbę: ')
+# d = input('wczytaj czwartą liczbę: ')
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# d = int(d)
+# if (a > b) & (c > d):
+#     print('liczba A jest wieksza od liczby B i liczba C jest wieksza od liczby D')
+# else:
+#     print('liczba A jest mniejsza od liczby B lub liczba C jest mniejsza od liczby D')
+
+
+# for a in range(0,10,1):
+#     print(a)
+
+
+# lista = ['a', 3, 5, 5.6, 10, 3]
+# for a in lista:
+#     print(a)
+# else:
+#     print('^Wyswietlone zostały elementy z listy^')
+
+
+# licznik = 0
+# while licznik != 10:
+#     print(licznik)
+#     licznik = licznik + 1
+# else:
+#     print('Wszystko git')
+
+
+# lista = [2 , 3, 4, 5 ,6, 7, 8, 9, 1]
+# a = input('Podaj liczbę: ')
+# licznik = 0
+# while licznik != len(lista):
+#     if int(a) - lista[licznik] == 0:
+#         print('liczba ' + str(a) + '-' + str(lista[licznik]) + ' = 0')
+#         break
+#     else:
+#         licznik = licznik + 1
+# else:
+#     print('żadna z wartości nie dała odpowiedniego wyniku')
+
+
+# lista1 = [2 , 3, 4, 5 ,6, 7]
+# lista2 = [3 , 5, 4, 8, 6, 10]
+# suma =[]
+# for a in lista1:
+#     for b in lista2:
+#         wynik = a + b
+#         suma.append(wynik)
+#     print(suma)
+
+
+# a = input('Wczytaj 1 liczbę całkowitą: ')
+# b = input('Wczytaj 2 liczbę całkowitą: ')
+# try:
+#     a = int(a)
+#     b = int(b)
+#     wynik = a/b
+#     print(wynik)
+# except ZeroDivisionError:
+#     print('Nie dziel cholero przez zero!!! ')
+# except ValueError:
+#     print('Nie wczytano liczby calkowitej!!! ')
+
+# lista=[2, 4, 6, 8, 10]
+# tab=['siemson','naura','informatyka']
+# slownik1 = {'klucz1': lista[0] ,'klucz2': 'siema ziomson'}
+# slownik2 = {'klucz1': lista[4] ,'klucz2': 'elton john','klucz3':tab[1]}
+# print(slownik1['klucz1'])
+# print(slownik1['klucz2'])
+# print(slownik2['klucz2'])
+# print(slownik2['klucz1'])
+# print(slownik2['klucz3'],slownik1['klucz2'])
+# slownik1['klucz10']='piachonski'
+# print(slownik1)
+
+
+
+
+
+
